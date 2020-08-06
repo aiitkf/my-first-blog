65a90958aa173d8783d95c7c21bfe218d78f33be
Fujikawa K <hoge@example.com>
2020-08-07 03:40:52 +0900

    merged

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post2/<int:pk>/', views.post_detail2, name='post_detail2'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+        # グラフ描画
+    path('plot/', views.get_svg, name='plot'),
+]
